03b45fdb6d4479583cd90e07cd7af9d70e9e8a2d
Shakibul Islam <shakibulislam684@gmail.com>
2023-05-03 02:43:05 +0600

    vercel config file added

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const port =process.env.PORT || 5000;
 app.get('/', (req, res) => {
   res.send('chef recipe hunter server side is running')
 })
-app.get('/alldata', (req, res) => {
-  res.send('hello');
-})
+
 
 app.listen(port, () => {
   console.log(`chef recipe hunter server side is running on port ${port}`)
