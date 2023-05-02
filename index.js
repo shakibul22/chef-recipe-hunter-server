5719dca3aa517d06f2e55a1a880f4c503524df1d
Shakibul Islam <shakibulislam684@gmail.com>
2023-05-03 04:59:21 +0600

    installed cors

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express')
-const app = express()
+const cors=require("cors");
+
+const app = express();
+app.use(cors());
 const port =process.env.PORT || 5000;
 
+const detailsData=require("./data.json");
+const details=require("./details.json");
+
 app.get('/', (req, res) => {
-  res.send('chef recipe hunter server side is running')
+  res.json('chef recipe hunter server side is running')
+})
+app.get('/allData', (req, res) => {
+  res.send(detailsData);
+})
+app.get('/details', (req, res) => {
+  res.send(details);
 })
 
 
 app.listen(port, () => {
-  console.log(`chef recipe hunter server side is running on port ${port}`)
+  console.log(`chef recipe hunter server side is running on port  ${port}`)
 })
